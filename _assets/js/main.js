78da6ed4c40ec8847bcddce7f3328316520fffe0
Rob Larsen <rob@drunkenfist.com>
2013-02-14 15:29:06 -0500

    Canvas introduced

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,19 @@
-
+;(function(window, $){
+    "use strict";    
+ 
+    // Check to see if our global is available as a member of window; if it is, our namespace root exists; if not, we'll create it.
+    var react = window.react = (typeof window.react !== "undefined") ? window.react : {};
+ 
+    (react.dataviz = function(){
+        return{
+            _init : function(){
+               var ctx = document.getElementById("circle").getContext("2d");
+               ctx.beginPath();
+               ctx.arc(400, 150, 75, 0, Math.PI*2, true); 
+               ctx.fillStyle = "#fe57a1";
+               ctx.closePath();
+               ctx.fill();
+            }
+        }
+    }())._init();
+}(window, jQuery));
