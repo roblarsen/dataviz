2888fe9c17eaf786c87afc71a7baeca059b9ce71
Rob Larsen <rob@drunkenfist.com>
2013-02-19 12:35:08 -0500

    adding a highstock example

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -7,12 +7,83 @@
     (react.dataviz = function(){
         return{
             _init : function(){
+               /*
+               Initial Canvas Demo
+                */
                var ctx = document.getElementById("circle").getContext("2d");
                ctx.beginPath();
                ctx.arc(400, 150, 75, 0, Math.PI*2, true); 
                ctx.fillStyle = "#fe57a1";
                ctx.closePath();
                ctx.fill();
+               /*
+               Highstock demo
+                */
+               var seriesOptions = [],
+                   yAxisOptions = [],
+                   seriesCounter = 0,
+                   names = ['AAPL', 'BBRY'],
+                   colors = Highcharts.getOptions().colors;
+
+  $.each(names, function(i, name) {
+
+    $.getJSON('../data/'+names[i].toLowerCase()+'.json', function(data) {
+      for (var j = 0, len = data.length; j >len; j++) {
+        data[i][0] = Date.parse( data[i][0] ).getTime();
+      }
+      data = data.reverse();
+      seriesOptions[i] = {
+        name: name,
+        data: data
+      };
+
+      // As we're loading the data asynchronously, we don't know what order it will arrive. So
+      // we keep a counter and create the chart when all the data is loaded.
+      seriesCounter++;
+
+      if (seriesCounter == names.length) {
+        createChart();
+      }
+    });
+  });
+
+
+
+  // create the chart when all data is loaded
+  function createChart() {
+
+    var chart = new Highcharts.StockChart({
+        chart: {
+            renderTo: 'highstock'
+        },
+        yAxis: {
+          labels: {
+            formatter: function() {
+              return (this.value > 0 ? '+' : '') + this.value + '%';
+            }
+          },
+          plotLines: [{
+            value: 0,
+            width: 2,
+            color: 'silver'
+          }]
+        },
+        
+        plotOptions: {
+          series: {
+            compare: 'percent'
+          }
+        },
+        
+        tooltip: {
+          pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+          valueDecimals: 2
+        },
+        
+        series: seriesOptions
+    });
+  }
+
             }
         }
     }())._init();
